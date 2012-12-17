765080a8f6a1ae11b9876fe9d20ed5f75b791dd2
Mnkras <mnkras@gmail.com>
2012-12-17 13:05:15 -0500

    more examples

--- api/routes/hello.php
+++ api/routes/hello.php
@@ -2,6 +2,12 @@
 
 class HelloApiRouteController extends ApiRouteController {
 
+	/**
+	 * Main function
+	 * @param string $name
+	 * @return void
+	 * @url /hello/:name
+	 */
 	public function run($name = false) {
 		switch (API_REQUEST_METHOD) {
 			case 'GET':
@@ -15,6 +21,23 @@ class HelloApiRouteController extends ApiRouteController {
 				$this->setCode(405);
 				$this->respond(array('error' => 'Method Not Allowed'));
 		}
+	}
+
+	/**
+	 * Test public function
+	 * @return void
+	 * @url /hello/world
+	 */
+	public function world() { //since this is a public function it can be access directly via the API
+		$this->respond('This is the `world` function!');
+	}
+
+	/**
+	 * Test private function
+	 * @return void
+	 */
+	private function underworld() { //since this is a private function it cannot be called directly by the API and can only be called from within this file.
+		$this->respond('This is the `underworld` function!');//you should never see this....
 	}
 
 }
